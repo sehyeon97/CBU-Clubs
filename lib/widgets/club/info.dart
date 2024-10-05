d55d6b2b8730cda84bd159349cc7be1e5beffb14
nsdevries03 <nathansamuel.devries@calbaptist.edu>
2024-10-05 14:08:47 -0700

    add firebase

--- lib/widgets/club/info.dart
+++ lib/widgets/club/info.dart
@@ -4,6 +4,8 @@ import 'package:club/providers/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ClubInfo extends ConsumerWidget {
   const ClubInfo({
     super.key,
@@ -16,6 +18,18 @@ class ClubInfo extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final availableClubs = ref.read(availableClubsProvider.notifier);
     final usersClubs = ref.read(userProvider.notifier);
+
+    Future<void> addClub(Club club) async {
+      final CollectionReference userRef = FirebaseFirestore.instance.collection('users');
+      //await userRef.doc('Nathan DeVries').collection('clubs').add(club.toMap());
+      CollectionReference clubs = FirebaseFirestore.instance.collection('clubs');
+    
+      try {
+        await clubs.add(club.toMap());
+      } catch (e) {
+        print("Failed to add club: $e");
+      }
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -30,6 +44,7 @@ class ClubInfo extends ConsumerWidget {
           const SizedBox(height: 20),
           ElevatedButton(
               onPressed: () {
+                addClub(club);
                 usersClubs.addClub(club);
                 availableClubs.removeClub(club);
                 Navigator.of(context).pop();
